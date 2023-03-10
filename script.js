b374894c5d739c2c4a8dc2a56c3ad3d3575c2373
sometimemecry <songphon.yy@hotmail.com>
2023-03-10 20:04:45 +0700

    Create an Age Calculator

---
+++ script.js
@@ -0,0 +1,4 @@
+let myBirthYear = 1998;
+let futureYear = 2030;
+let possibleYear = futureYear - myBirthYear;
+console.log(`If you were born in ${myBirthYear}` + ` then in ${futureYear}` + ` you'll be ${possibleYear} (depending on  the month in 2030)`);
