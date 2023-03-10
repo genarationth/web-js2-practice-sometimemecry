05e50ccabac0afdfaee6e2f25c0bb0a6d196c011
sometimemecry <songphon.yy@hotmail.com>
2023-03-10 20:11:15 +0700

    Create an information card

---
+++ script1.js
@@ -0,0 +1,4 @@
+const myName = 'Songphon';
+const myCity = 'Buriram Thailand';
+const myHobby = 'playing computer games';
+console.log(`My name is ${myName}. I live in ${myCity} and when I have a free time I'm ${myHobby}`);
